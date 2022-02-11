ae01e261da7a683e1853d8baa39ce1291be6d5c8
SHRUTI-SINGH16 <91374600+SHRUTI-SINGH16@users.noreply.github.com>
2022-02-11 12:43:00 +0530

    commit

---
+++ c++/function-print-num.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+void print_num(int num){
+    cout<<num<<endl;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    print_num(n);
+
+    
+    return 0;
+}
